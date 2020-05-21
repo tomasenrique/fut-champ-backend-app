3d65ef00cde3ba0fa87d209aad5ca5176bc5622f
Tomas Estrada <tomasenriquea@hotmial.com>
2020-05-21 15:31:28 +0200

    Cambio de BBDD de local a remota.

--- src/main/java/app/controladores/PartidoController.java
+++ src/main/java/app/controladores/PartidoController.java
@@ -1,8 +1,78 @@
 package app.controladores;
 
-//@RestController
-//@RequestMapping(path = "/api/futchamp/partido")
+import app.entidades.Calendario;
+import app.entidades.Equipo;
+import app.entidades.League;
+import app.entidades.Partido;
+import app.repositoryCRUD.CalendarioRepository;
+import app.repositoryCRUD.EquipoRepository;
+import app.repositoryCRUD.PartidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping(path = "/api/futchamp/partido")
 public class PartidoController {
+
+    @Autowired
+    private PartidoRepository partidoRepository;
+
+    @Autowired
+    private EquipoRepository obtenerDatosEquipoRepository;
+
+    @Autowired
+    private CalendarioRepository obtenerDatosCalendarioRepository;
+
+
+    @PostMapping("/agregar")
+    public ResponseEntity<Partido> agregarPartido(@RequestBody Partido partido) {
+        // Obtiene los datos del equipo local
+        Equipo buscandoEquipoLocal = obtenerDatosEquipoRepository.findEquipoByName(partido.getLocal().getName());
+        // Obtiene los datos del equipo visitante
+        Equipo buscandoEquipoVisitante = obtenerDatosEquipoRepository.findEquipoByName(partido.getVisitante().getName());
+        // Obtiene los datos de un calendario por medio de su nombre y fecha inicial
+        Calendario buscandoCalendario = obtenerDatosCalendarioRepository.findCalendarioByLeagueAndFecha(partido.getCalendario().getLeague(), partido.getCalendario().getFecha());
+
+        if (buscandoEquipoLocal != null) {
+            if (buscandoEquipoVisitante != null) {
+                if (buscandoCalendario != null) { // OJO CON LA FECHA QUE PARECE QUE HAY PROBLEMA ???
+                    partido.setLocal(buscandoEquipoLocal);
+                    partido.setVisitante(buscandoEquipoVisitante);
+                    partido.setCalendario(buscandoCalendario);
+                    partidoRepository.save(partido);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(partido);
+                } else {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se pudo agregar el partido, no hay calendario");
+                }
+            } else {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se agrego el partido, no hay equipo visitante");
+            }
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No se agrego el partido, no hay equipo local");
+        }
+    }
+
+    // =================================================================================================================
+
+    // Muestra todos partidos que hay en la BBDD
+    @GetMapping("/mostrar")
+    public Iterable<Partido> mostrarPartidos() {
+        try {
+            return partidoRepository.findAll();
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No hay partidos registrados.");
+        }
+
+    }
+
+
+
+
+    // =================================================================================================================
 
 
 }
--- src/main/java/app/repositoryCRUD/CalendarioRepository.java
+++ src/main/java/app/repositoryCRUD/CalendarioRepository.java
@@ -1,9 +1,22 @@
 package app.repositoryCRUD;
 
 import app.entidades.Calendario;
+import app.entidades.League;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface CalendarioRepository extends CrudRepository<Calendario, Long> {
+
+    // Muestra una lista de calendarios con los nombres de las leagues que generaron los partidos y su fecha y hora inical
+    List<Calendario> findCalendarioByLeague(String nombreLeague);
+
+    // Muestra solo un calendario ubicado por su nombre de liga y la fecha inicial.
+    Calendario findCalendarioByLeagueAndFecha(String nombreLeague, LocalDate fechaInicio);
+
+
 }
